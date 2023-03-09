a7e7aaa043f9f9f7324bc4912abee544fd0c753f
jin <295588728@qq.com>
2023-03-10 00:21:18 +0800

    维吉尼亚密码 - 运行成功

--- cipherscript/cipher_vigenere.py
+++ cipherscript/cipher_vigenere.py
@@ -6,7 +6,7 @@
 # @File : cipher_vigenere.py
 # @Project : pythonProject3
 # @IDE: PyCharm 2022.3.2
-# @Description : 维吉尼亚加密解密 - 运行成功
+# @Description : 维吉尼亚密码 - 运行成功
 
 def encrypt_vigenere(plaintext, key):
     ciphertext = ''
@@ -23,6 +23,7 @@ def encrypt_vigenere(plaintext, key):
         else:
             ciphertext += char
     return ciphertext
+
 
 def decrypt_vigenere(ciphertext, key):
     plaintext = ''
@@ -41,9 +42,11 @@ def decrypt_vigenere(ciphertext, key):
     return plaintext
 
 
-plaintext = "The quick brown fox jumps over the lazy dog."
-key = "KKKKKK"
-ciphertext = encrypt_vigenere(plaintext, key)
-print(ciphertext) # prints "Uvs osckv qhdt huf ivyls oydf zpw."
-decrypted_text = decrypt_vigenere(ciphertext, key)
-print(decrypted_text) # prints "The quick brown fox jumps over the lazy dog."
+if __name__ == '__main__':
+    # --- 调用测试 ---
+    plaintext = "The quick brown fox jumps over the lazy dog."
+    key = "ABCDEF"
+    ciphertext = encrypt_vigenere(plaintext, key)
+    print(ciphertext) # prints "Uvs osckv qhdt huf ivyls oydf zpw."
+    decrypted_text = decrypt_vigenere(ciphertext, key)
+    print(decrypted_text) # prints "The quick brown fox jumps over the lazy dog."
